9816f88935094e2356fefdc207d6f9b10e8fda08
wley3337 <wley3337@gmail.com>
2019-08-25 20:35:29 -0400

    removed the testing component

--- src/components/TryOut.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { TestPropTypes } from '../interfaces/TestPropTypes'
-
-const TryOut: React.FC<TestPropTypes>= (props)=>{
-    const {name, num} = props
-    return <div>This is the {name} and number {num} </div>
-}
-
-export default TryOut
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import TryOut   from '../components/TryOut'
 import { LogIn } from '../components/LogIn';
 
 const App = () =>  {
-  
+
   return (
     <div className="App">
-      <TryOut  name="Me" num={42}/>
       <LogIn />
     </div>
   );
